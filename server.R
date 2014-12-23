64ea9df8bddab80fe5bd3a80f3bc7a9edc5bb6f8
Eyðun Nielsen <en@hagstova.fo>
2014-12-23 03:25:20 +0000

    "Initial commit"

---
+++ server.R
@@ -0,0 +1,41 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyServer(function(input, output, session) {
+  observe({
+    
+    
+    label_children <- paste0("0-",input$age[1]-1)
+    label_elders <- paste0(input$age[2]+1, "+")
+    label_both <- paste0(label_children, " and ", label_elders)
+    
+    
+    r_options <- list()
+    r_options[[label_children]] <- "children"
+    r_options[[label_elders]] <- "elders"
+    r_options[[label_both]] <- "both"
+    
+    # Set the choices
+    updateRadioButtons(session, "depgrps", choices = r_options, selected = input$depgrps, inline = T)
+    
+    
+    
+    teljari <- switch(
+      input$depgrps,
+      children = label_children,
+      elders = label_elders,
+      both = paste0("(",label_children, " and ", label_elders, ")")
+    )    
+    nevnari <- paste0("'",input$age[1], "-", input$age[2],"'")
+    
+    output$textur <- renderText({ 
+      paste0("Dependency ratio to calculate: ", teljari, " / ", nevnari )
+    })
+  })
+})
---
+++ ui.R
@@ -0,0 +1,38 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+
+shinyUI(fluidPage(
+
+  # Application title
+  titlePanel("Dependency ratio (ósjálvbjargnis lutfallið) :-)"),
+
+  # Sidebar with a slider input for workforce age range
+  sidebarLayout(position='right',
+    sidebarPanel(
+      
+      radioButtons("depgrps", "Dependency groups:",
+                         c("0-15" = "children",
+                           "67+" = "elders",
+                           "0-15 and 67+" = "both"),
+                           selected = "elders",
+                           inline = T),
+      sliderInput("age",
+                  "Workforce age range",
+                  min = 1,
+                  max = 99,
+                  value = c(16,66))
+    ),
+    
+
+# Show the plot
+    mainPanel(
+      textOutput("textur")
+    )
+  )
+))
